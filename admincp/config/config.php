3a734fa127318c78a40026d79e1a1a3d193a7c6b
hieudaddy <93580415+hieudaddy@users.noreply.github.com>
2024-08-26 00:25:00 +0700

    Update config.php

--- admincp/config/config.php
+++ admincp/config/config.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost","root","","web_mysqli");
+$mysqli = new mysqli("dbhades.mysql.database.azure.com","lehieu","Hieztlyn23","web_mysqli");
 $mysqli->set_charset("utf8");
 
 // Check connection
@@ -7,5 +7,4 @@ if ($mysqli -> connect_errno) {
   echo "Kết nối MYSQLi lỗi: " . $mysqli -> connect_error;
   exit();
 }
-
 ?>
